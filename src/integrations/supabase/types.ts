095ca964dbdb4d76db59e9e3b4342b7baf63c3de
Lucas Greenwell <54864050+lucasgreenwell@users.noreply.github.com>
2025-03-12 02:36:07 -0500

    Update types.ts

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -296,21 +296,18 @@ export type Database = {
         Row: {
           created_at: string | null
           id: string
-          title: string | null
           updated_at: string | null
           user_id: string
         }
         Insert: {
           created_at?: string | null
           id?: string
-          title?: string | null
           updated_at?: string | null
           user_id: string
         }
         Update: {
           created_at?: string | null
           id?: string
-          title?: string | null
           updated_at?: string | null
           user_id?: string
         }
